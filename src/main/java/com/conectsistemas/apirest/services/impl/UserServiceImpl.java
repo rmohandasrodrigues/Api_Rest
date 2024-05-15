5d57e1c4d536ebb360ac17db5fe756be01221a76
rmohandasrodrigues <rmohandasrodrigues@gmail.com>
2024-05-14 23:40:17 -0300

    Classe user service  impl findById

---
+++ src/main/java/com/conectsistemas/apirest/services/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.conectsistemas.apirest.services.impl;
+
+import com.conectsistemas.apirest.Repository.UserRepository;
+import com.conectsistemas.apirest.domain.User;
+import com.conectsistemas.apirest.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElse(null);
+    }
+}
