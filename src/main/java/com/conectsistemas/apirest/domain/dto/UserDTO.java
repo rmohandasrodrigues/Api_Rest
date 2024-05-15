2522aee35c10c5654897fcce5c2fd84393e75abe
rmohandasrodrigues <rmohandasrodrigues@gmail.com>
2024-05-15 01:07:50 -0300

    Classe user dto.

---
+++ src/main/java/com/conectsistemas/apirest/domain/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package com.conectsistemas.apirest.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String nome;
+    private String email;
+    private String password;
+
+}
