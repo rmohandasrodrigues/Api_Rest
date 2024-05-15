20737767c46f8e1f9215068ec2b2039813144239
rmohandasrodrigues <rmohandasrodrigues@gmail.com>
2024-05-14 21:33:34 -0300

    Classe usuario.

---
+++ src/main/java/com/conectsistemas/apirest/domain/User.java
@@ -0,0 +1,21 @@
+package com.conectsistemas.apirest.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
