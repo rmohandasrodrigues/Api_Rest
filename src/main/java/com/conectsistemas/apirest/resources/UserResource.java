bb337cb640af53666749d31bee27e75dae7a23fb
rmohandasrodrigues <rmohandasrodrigues@gmail.com>
2024-05-14 23:40:42 -0300

    Classe user resource findById

---
+++ src/main/java/com/conectsistemas/apirest/resources/UserResource.java
@@ -0,0 +1,23 @@
+package com.conectsistemas.apirest.resources;
+
+import com.conectsistemas.apirest.domain.User;
+import com.conectsistemas.apirest.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = "{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(userService.findById(id));
+    }
+}
