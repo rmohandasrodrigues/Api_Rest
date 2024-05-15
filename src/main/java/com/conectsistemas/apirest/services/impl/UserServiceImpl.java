c7d4e1870c05bb61feff9300c7578138926b86d2
rmohandasrodrigues <rmohandasrodrigues@gmail.com>
2024-05-15 00:59:12 -0300

    Tratamento de exceção para objeto não encontrado.

--- src/main/java/com/conectsistemas/apirest/services/impl/UserServiceImpl.java
+++ src/main/java/com/conectsistemas/apirest/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.conectsistemas.apirest.services.impl;
 import com.conectsistemas.apirest.Repository.UserRepository;
 import com.conectsistemas.apirest.domain.User;
 import com.conectsistemas.apirest.services.UserService;
+import com.conectsistemas.apirest.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
     }
 }
