931b4519085829a36657f1b307ba57bd4979ff3e
rmohandasrodrigues <rmohandasrodrigues@gmail.com>
2024-05-27 16:26:25 -0300

    EndPoint findAll para user

--- src/main/java/com/conectsistemas/apirest/services/UserService.java
+++ src/main/java/com/conectsistemas/apirest/services/UserService.java
@@ -2,7 +2,10 @@ package com.conectsistemas.apirest.services;
 
 import com.conectsistemas.apirest.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }
